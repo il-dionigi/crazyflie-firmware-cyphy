57264e0c86648872ecd98d1af3a5c22147b8f82f
bitcraze <bitcraze@bitcraze-vm.unassigned-domain>
2018-07-25 00:33:24 +0200

    wolfssl

--- src/modules/src/consoleComm.c
+++ src/modules/src/consoleComm.c
@@ -79,89 +79,6 @@ static char radioAddress[16] = "XXXXXXXXXXXXXXX\0";
 static char radioChannel[3] = "XX\0";
 static char radioDatarate[2] = "X\0";
 
-//Encryption stuff
-EVP_CIPHER_CTX en, de;
-static char encryptedMessage[128];
-
-/* 8 bytes to salt the key_data during key generation. This is an example of
-   compiled in salt. We just read the bit pattern created by these two 4 byte
-   integers on the stack as 64 bits of contigous salt material -
-   ofcourse this only works if sizeof(int) >= 4 */
-unsigned int salt[] = {12345, 54321};
-unsigned char *key_data;
-int key_data_len, i;
-/*char *input[] = {"a", "abcd", "this is a test", "this is a bigger test",
-				 "\nWho are you ?\nI am the 'Doctor'.\n'Doctor' who ?\nPrecisely!",
-				 NULL};*/
-
-int aes_init(unsigned char *key_data, int key_data_len, unsigned char *salt, EVP_CIPHER_CTX *e_ctx,
-             EVP_CIPHER_CTX *d_ctx)
-{
-  int i, nrounds = 5;
-  unsigned char key[32], iv[32];
-
-  /*
-   * Gen key & IV for AES 256 CBC mode. A SHA1 digest is used to hash the supplied key material.
-   * nrounds is the number of times the we hash the material. More rounds are more secure but
-   * slower.
-   */
-  i = EVP_BytesToKey(EVP_aes_256_cbc(), EVP_sha1(), salt, key_data, key_data_len, nrounds, key, iv);
-  if (i != 32) {
-    printf("Key size is %d bits - should be 256 bits\n", i);
-    return -1;
-  }
-
-  EVP_CIPHER_CTX_init(e_ctx);
-  EVP_EncryptInit_ex(e_ctx, EVP_aes_256_cbc(), NULL, key, iv);
-  EVP_CIPHER_CTX_init(d_ctx);
-  EVP_DecryptInit_ex(d_ctx, EVP_aes_256_cbc(), NULL, key, iv);
-
-  return 0;
-}
-
-/*
- * Encrypt *len bytes of data
- * All data going in & out is considered binary (unsigned char[])
- */
-unsigned char *aes_encrypt(EVP_CIPHER_CTX *e, unsigned char *plaintext, int *len)
-{
-  /* max ciphertext len for a n bytes of plaintext is n + AES_BLOCK_SIZE -1 bytes */
-  int c_len = *len + AES_BLOCK_SIZE, f_len = 0;
-  unsigned char *ciphertext = malloc(c_len);
-
-  /* allows reusing of 'e' for multiple encryption cycles */
-  EVP_EncryptInit_ex(e, NULL, NULL, NULL, NULL);
-
-  /* update ciphertext, c_len is filled with the length of ciphertext generated,
-    *len is the size of plaintext in bytes */
-  EVP_EncryptUpdate(e, ciphertext, &c_len, plaintext, *len);
-
-  /* update ciphertext with the final remaining bytes */
-  EVP_EncryptFinal_ex(e, ciphertext+c_len, &f_len);
-
-  *len = c_len + f_len;
-  return ciphertext;
-}
-
-/*
- * Decrypt *len bytes of ciphertext
- */
-unsigned char *aes_decrypt(EVP_CIPHER_CTX *e, unsigned char *ciphertext, int *len)
-{
-  /* plaintext will always be equal to or lesser than length of ciphertext*/
-  int p_len = *len, f_len = 0;
-  unsigned char *plaintext = malloc(p_len);
-
-  EVP_DecryptInit_ex(e, NULL, NULL, NULL, NULL);
-  EVP_DecryptUpdate(e, plaintext, &p_len, ciphertext, *len);
-  EVP_DecryptFinal_ex(e, plaintext+p_len, &f_len);
-
-  *len = p_len + f_len;
-  return plaintext;
-}
-
-//end enc stuff
-
 
 void writeDroneData(char * str, int len) {
   if((currBufferLen + len) > 1024){
@@ -178,40 +95,18 @@ void writeDroneData(char * str, int len) {
  */
 static bool consoleCommSendMessage(void)
 {
-   unsigned int len = strlen(messageToPrint.data)+1;
-   char * cipherText;
-  cipherText = aes_encrypt(&en, (unsigned char *)messageToPrint.data, &len);
-  len = strlen(messageToPrint.data);
-  encryptedMessage[0] = "<";
-  memcpy(encryptedMessage+1, cipherText, len);
-  encryptedMessage[len+1] = ">";
-  encryptedMessage[len+2] = "\0";
-  len = len+3; //enc message has 3 more chars
-  unsigned int charsSent = 0;
-  int diff = 0;
-  while(charsSent < len){
-	  diff = len - charsSent;
-	  if (diff < 30){
-		  memcpy(messageToPrint.data, encryptedMessage+charsSent, diff);
-		  messageToPrint.data[diff+1] = '\0';
-		  messageToPrint.size = diff+1;
-	  }
-	  else {
-		  memcpy(messageToPrint.data, encryptedMessage+charsSent, 30);
-		  messageToPrint.size = 30;
-	  }
-	  messageToPrint.header = CRTP_HEADER(CRTP_PORT_CONSOLE, 1);
-	  if (crtpSendPacket(&messageToPrint) == pdTRUE)
-	  {
-		charsSent += 30;
-	  }
-	  else
-	  {
-		  //Error, we should light an LED here
-		return false;
-	  }
+
+  if (crtpSendPacket(&messageToPrint) == pdTRUE)
+  {
+	  messageToPrint.size = 0;
   }
-  messageToPrint.size = 0;
+  else
+  {
+	  //Error, we should light an LED here
+	return false;
+  }
+
+
   return true;
 }
 
@@ -330,18 +225,7 @@ void consoleCommInit()
   xTaskCreate(consoleCommTask, CONSOLE_COMM_TASK_NAME,
   			CONSOLE_COMM_TASK_STACKSIZE, NULL, CONSOLE_COMM_TASK_PRI, NULL);
 
-
-    key_data = "ThisIsMyKey";
-    key_data_len = strlen(key_data);
-
-    /* gen key and iv. init the cipher ctx object */
-    /*if (aes_init(key_data, key_data_len, (unsigned char *)&salt, &en, &de)) {
-      printf("Couldn't initialize AES cipher\n");
-      return -1;
-    }*/
-  aes_init(key_data, key_data_len, (unsigned char *)&salt, &en, &de);
   isInit = true;
-
   consoleCommPflush("console comm init!");
 }
 
